cb05acd420f6dd55f441ce8cf6e859e1577eabea
Liathe Najdawi <lnajdawi@memphis.edu>
2019-09-17 23:14:48 -0500

    Demo 5 update

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,39 @@
 class StaticPagesController < ApplicationController
   def welcome
+    features = [
+      'Choose from premade quizzes on a variety of topics',
+      'Make your own quizzes to customize your learning',
+      'Compare your scores with other users'
+    ]
+
+
     respond_to do |format|
-      format.html { render :welcome}
+      format.html { render :welcome, locals: {features: features}}
     end
   end
+  def question
+    answers = [
+      'a) A class should have only one reason to change.',
+      'b) Each developer on a team should have one main responsibility.',
+      'c) Every app will have one responsibility that is most important to users.'
+    ]
+
+    respond_to do |format|
+      format.html { render :question, locals: {answers: answers} }
+    end
+  end
+  def profile
+    respond_to do |format|
+      format.html { render :profile }
+    end
+  end
+
+  def about
+    respond_to do |format|
+      format.html { render :about}
+    end
+  end
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   get 'welcome', to: 'static_pages#welcome', as: 'welcome'
+  get 'about', to: 'static_pages#about', as: 'about'
+  get 'question', to: 'static_pages#question', as: 'question'
+    get 'profile', to: 'static_pages#profile', as: 'profile'
+  root to: redirect('/welcome', status: 302)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
